9efe493be610ee2716af1f2f815300d8d2e85663
Jhoyner González <jhoyner.gonzalez.porras@est.una.ac.cr>
2026-05-13 17:00:12 -0600

    Agregue el ejercicio del Laboratorio 5

---
+++ sesion11/Laboratorio5RadioFm/Laboratorio5RadioFm/NodoLocutor.h
@@ -0,0 +1,29 @@
+#ifndef NODOLOCUTOR_H
+#define NODOLOCUTOR_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace EIF201 {
+
+    struct NodoLocutor {
+        string nombre;
+        int turnosAsignados;
+        NodoLocutor* siguiente; 
+
+        NodoLocutor(const string& n)
+            : nombre(n), turnosAsignados(0), siguiente(nullptr) {
+            cout << "[NodoLocutor creado: " << n << "]" << endl;
+        }
+
+        ~NodoLocutor() {
+            cout << "[NodoLocutor destruido: " << nombre << "]" << endl;
+        }
+    };
+
+} 
+
+
+#endif
